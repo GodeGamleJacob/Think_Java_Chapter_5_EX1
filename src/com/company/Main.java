b4ed2049582033edee7fd442ef36f4c6a43875dd
GodeGamleJacob <89967283+GodeGamleJacob@users.noreply.github.com>
2021-09-14 09:27:25 +0200

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int number =9;
+
+        if (number > 0 && number < 10)  {
+
+        System.out.println("positive single digit number.");
+    }
+    }
+}
